c210f150513606d10bde3349f2439b83b8824552
slyalin <slyalin@f7bf301e-a539-0410-8f40-a4c9e81c2584>
2007-10-31 19:10:27 +0000

    Created a new branch based on branches/pretrunk #104 to fix global issue with multiple overloaded function templates (MSVS, GCC and ICC incompatibility on binding of names). And also to fix issue with imported names from STL library (such as pow, sin, cos etc).
    Each of common functions of Arageli namespace, for example, is_null, power, gcd, will be represented by a set of structure specializations (not functions) and one helper function template to fix the issue with early binding of functions templates.

---
+++ test/matrix_ops.hpp
@@ -0,0 +1,78 @@
+/*****************************************************************************
+
+    test/matrix_ops.hpp
+
+    This file is a part of the Arageli library test base.
+
+    Copyright (C) 1999--2007 Nikolai Yu. Zolotykh
+    REFERENCE ADDITIONAL COPYRIGHTS HERE
+    University of Nizhni Novgorod, Russia
+
+    The Arageli Library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    The Arageli Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
+    We are also open for dual licensing for the whole library or
+    for its particular part. If you are interested to get the library
+    in this way, i.e. not under the GNU General Public License,
+    please contact Arageli Support Service support.arageli@gmail.com.
+
+*****************************************************************************/
+
+/** \file matrix_ops.h
+    \brief Operations with the matrix.
+
+    Contains extended operations with matrix object that is necessary for testing
+    purposes.
+*/
+
+#ifndef __ta_matrix_ops_hpp__
+#define __ta_matrix_ops_hpp__
+
+
+#include "stdafx.hpp"
+
+
+using namespace Arageli;
+
+/// Create matrix in \f$T^{n \times m}\f$ with random elements not grater than max
+template <typename T, bool REFCNT>
+matrix<T, REFCNT> rand_matrix(size_t m, size_t n, const T& max);
+
+/**
+Create upper diagonal matrix \f$M \in T^{n \times n}\f$ with element 1 on the
+diagonal and with random elements the not grater than max on other
+positions.
+Note that \f$M\f$ is in hermite form and \f$det(M)=1\f$.
+*/
+template <typename T, bool REFCNT>
+matrix<T, REFCNT> rand_matrix_upper_diagonal(size_t n, const T& max);
+
+/**
+Shuffles input matrix \f$m\f$ by multiplying it on the upper diagonal
+random matrix acquired from rand_matrix_upper_diagonal(m.ncols(), max) call.
+Note that this operation does not change determinant of the source matrix.
+*/
+template <typename T, bool REFCNT>
+void shuffle(matrix<T, REFCNT> &m, const T& max);
+
+
+template <typename M>
+inline void output_map (const M& m)
+{
+    for(typename M::const_iterator i = m.begin(); i != m.end(); ++i)
+        tout << "[" << i->first << "] = " << i->second << "\n";
+}
+
+
+#endif /*__ta_matrix_ops_hpp__*/
+/* End of file matrix_ops.hpp */
