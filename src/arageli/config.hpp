346a26a2b9d5f75d8031e07fe0e9d9cf56794e90
bader <bader@f7bf301e-a539-0410-8f40-a4c9e81c2584>
2007-12-23 18:28:09 +0000

    Added initial verstion of autotools scripts.

--- src/arageli/config.hpp
+++ src/arageli/config.hpp
@@ -1,186 +1,106 @@
-/*****************************************************************************
+/* src/arageli/config.hpp.  Generated from config.hpp.in by configure.  */
+/* src/arageli/config.hpp.in.  Generated from configure.in by autoheader.  */
 
-    config.hpp
+/* Define to 1 if you have the `floor' function. */
+/* #undef HAVE_FLOOR */
 
-    This file is a part of Arageli library.
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
 
-    Copyright (C) 1999--2006 Nikolai Yu. Zolotykh
-    Copyright (C) 2005--2007 Sergey S. Lyalin
-    University of Nizhni Novgorod, Russia
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
 
-    The Arageli Library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License version 2
-    as published by the Free Software Foundation.
+/* Define to 1 if you have the <malloc.h> header file. */
+#define HAVE_MALLOC_H 1
 
-    The Arageli Library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+/* Define to 1 if you have the `memmove' function. */
+#define HAVE_MEMMOVE 1
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
 
-    We are also open for dual licensing for the whole library or
-    for its particular part. If you are interested to get the library
-    in this way, i.e. not under the GNU General Public License,
-    please contact Arageli Support Service support.arageli@gmail.com.
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
 
-*****************************************************************************/
+/* Define to 1 if you have the `pow' function. */
+/* #undef HAVE_POW */
 
+/* Define to 1 if your system has a GNU libc compatible `realloc' function,
+   and to 0 otherwise. */
+#define HAVE_REALLOC 1
 
-#ifndef _ARAGELI_config_hpp_
-#define _ARAGELI_config_hpp_
+/* Define to 1 if you have the `sqrt' function. */
+/* #undef HAVE_SQRT */
 
-/**
-    \file config.hpp
-    \brief Several configuration switches.
+/* Define to 1 if stdbool.h conforms to C99. */
+#define HAVE_STDBOOL_H 1
 
-    Some macros from this file can be defined through the compiler command
-    line. If so, the values for such macros from this file don't have
-    an effect.
-*/
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
 
-/// Version of the library.
-#define ARAGELI_VERSION "2.2.8"
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
 
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
 
-/// Controls whether cpp-files would be included into corresponding hpp-files.
-/** If this macro is defined, cpp-files with template implementations will be
-    included into corresponding hpp-files to be visible to the compiler when
-    the user's code is being compiled. */
-#define ARAGELI_INCLUDE_CPP_WITH_EXPORT_TEMPLATE
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
 
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
 
-// #define ARAGELI_PLATFORM_OPTIMIZATION_1
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
 
-// #define ARAGELI_PLATFORM_OPTIMIZATION_2
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
 
+/* Define to 1 if the system has the type `_Bool'. */
+#define HAVE__BOOL 1
 
+/* Name of package */
+#define PACKAGE "arageli"
 
-#ifndef ARAGELI_DEBUG_LEVEL
-    /// The debug level.
-    /** Activates assert macros with level-number less then this value.
-        So, zero deactivates all assert macros and value 4 activates
-        all asserts. */
-    #ifdef NDEBUG
-        #define ARAGELI_DEBUG_LEVEL        1
-    #else
-        #define ARAGELI_DEBUG_LEVEL        3
-    #endif
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "support.arageli@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Arageli"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Arageli 2.59"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "arageli"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.59"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if your <sys/time.h> declares `struct tm'. */
+/* #undef TM_IN_SYS_TIME */
+
+/* Version number of package */
+#define VERSION "2.59"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
 #endif
 
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
 
-/// Turn on some debug output command to std::cout inlined to internal code.
-//#define ARAGELI_INLINE_DEBUG_OUTPUT
+/* Define to rpl_realloc if the replacement function should be used. */
+/* #undef realloc */
 
-/// Turn on advanced multiplication methods
-//#define ARAGELI_ENABLE_POLLARD_MULT
-#define ARAGELI_ENABLE_KARATSUBA_MULT
-
-/// If this macro is defined then assertions throw exception assert_failed.
-//#define ARAGELI_ASSERT_THROW_EXCEPTION
-
-
-/// Turn on tracing subsystem of the library.
-/**  See exception.hpp for more details. */
-//#define ARAGELI_TRACING
-
-
-/// Turn on the use of __assume intrinsic instead empty ASSERT substitution.
-/** Macros from the ARAGELI_ASSERT family is expanded to nothing if the debug
-    level is low enough. Defining this macro we allow using __assume intrinsic
-    instead an empty expantion in those cases.
-    WARNING! Temporary this feature can't be turned on because of
-    not compatibility with ARAGELI_DEBUG_EXEC macros family. */
-//#define ARAGELI_ASSERT_ASSUME
-
-
-/// The number of iterations in is_prime_small_primes_division function.
-#define ARAGELI_IS_PRIME_SMALL_PRIMES_DIVISION_NUMBER 20
-
-
-#if defined(_MSC_VER) && defined(_WIN32)
-    #define ARAGELI_INT64_SUPPORT
-#endif
-
-
-// Please check whether it is correct or not. And only after that uncomment.
-#ifdef __linux
-    #define ARAGELI_LONG_LONG_SUPPORT
-#endif
-
-
-/// Enables support of signed __int64 and unsigned __int64 types.
-//#define ARAGELI_INT64_SUPPORT_ENABLE
-
-/// Enables support of unsigned signed long long and unsigned long long.
-//#define ARAGELI_LONG_LONG_SUPPORT_ENABLE
-
-/// Disables support of signed __int64 and unsigned __int64 types.
-//#define ARAGELI_INT64_SUPPORT_DISABLE
-
-/// Disables support of unsigned signed long long and unsigned long long.
-//#define ARAGELI_LONG_LONG_SUPPORT_DISABLE
-
-#if defined(ARAGELI_INT64_SUPPORT_ENABLE) && defined(ARAGELI_INT64_SUPPORT_DISABLE)
-    #error Both macros ARAGELI_INT64_SUPPORT_DISABLE and    \
-        ARAGELI_INT64_SUPPORT_ENABLE are defined.
-#endif
-
-
-#if defined(ARAGELI_LONG_LONG_SUPPORT_ENABLE) && defined(ARAGELI_LONG_LONG_SUPPORT_DISABLE)
-    #error Both macros ARAGELI_LONG_LONG_SUPPORT_DISABLE and    \
-        ARAGELI_LONG_LONG_SUPPORT_ENABLE are defined.
-#endif
-
-
-#ifdef ARAGELI_INT64_SUPPORT_ENABLE
-    #define ARAGELI_INT64_SUPPORT
-#elif defined(ARAGELI_INT64_SUPPORT_DISABLE) && defined(ARAGELI_INT64_SUPPORT)
-    #undef ARAGELI_INT64_SUPPORT
-#endif
-
-
-#ifdef ARAGELI_LONG_LONG_SUPPORT_ENABLE
-    #define ARAGELI_LONG_LONG_SUPPORT
-#elif defined(ARAGELI_LONG_LONG_SUPPORT_DISABLE) && defined(ARAGELI_LONG_LONG_SUPPORT)
-    #undef ARAGELI_LONG_LONG_SUPPORT
-#endif
-
-
-/// Disables some negligible warnings under some systems.
-/** If you modified the library, you likely want to not define
-    this macro to see all warnings occur for some new feature. */
-#define ARAGELI_DISABLE_PARTICULAR_COMPILER_WARNINGS
-
-
-/// Allows the compiler automatically link the Arageli library.
-/** It's useful when the user's code is being compiled.
-    WARNING! This option temporary isn't allowed. */
-//#define ARAGELI_USE_AUTOLINK
-
-
-#ifdef ARAGELI_USE_AUTOLINK_ENABLE
-    #define ARAGELI_USE_AUTOLINK
-#elif defined(ARAGELI_USE_AUTOLINK_DISABLE) && defined(ARAGELI_USE_AUTOLINK)
-    #undef ARAGELI_USE_AUTOLINK
-#endif
-
-
-#ifdef ARAGELI_USE_AUTOLINK
-    #define ARAGELI_LIB_NAME arageli
-    #include "auto_link.hpp"
-#endif
-
-
-// Be careful! The next pragmas disables some warnings for the whole library
-// and for all code that uses the library.
-#ifdef ARAGELI_DISABLE_PARTICULAR_COMPILER_WARNINGS
-    #pragma warning (disable : 4996)
-    #pragma warning (disable : 4800)
-    #pragma warning (disable : 4244)
-#endif
-
-
-#endif  //  #ifndef _ARAGELI_config_hpp_
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
