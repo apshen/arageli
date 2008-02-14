171a8f5819f79c68db6c62c423096a51c05a9bcb
bader <bader@f7bf301e-a539-0410-8f40-a4c9e81c2584>
2008-02-14 11:02:04 +0000

    Merging changes from branches/multiplication. Improved memory management in pollard and karatsuba tests. Linux build system also was improved.

--- branches/pretrunk/src/arageli/karatsuba.cpp
+++ branches/pretrunk/src/arageli/karatsuba.cpp
@@ -47,6 +47,7 @@ namespace Arageli
 
 /**
 Preliminary conditions: w[n+m+1] = 0!
+                        m >= n
 */
 template <typename N,typename T>
 T do_mult_karatsuba(const N *u, const N *v, N *w, N *t, T m, T n, T threshold)
