6254d07b05e30344d8ceb0c9a2498e924bfe9b17
slyalin <slyalin@f7bf301e-a539-0410-8f40-a4c9e81c2584>
2008-02-16 20:06:49 +0000

    Added a pattern for a source file based on the pattern from Arageli itself.

---
+++ branches/msvs_integration/tool/msvs_integration/src/_pattern/_pattern.cpp
@@ -0,0 +1,41 @@
+/*****************************************************************************
+
+    _pattern.cpp
+
+    This file is a part of the Arageli MSVS intergation infrastructure,
+    a part of Arageli library.
+
+    Copyright (C) 2007--2008 Andrey M. Kamaev
+    Copyright (C) 2007--2008 Sergey S. Lyalin
+    REFERENCE ADDITIONAL COPYRIGHTS HERE
+    University of Nizhni Novgorod, Russia
+
+    The Arageli Library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    The Arageli Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
+    We are also open for dual licensing for the whole library or
+    for its particular part. If you are interested to get the library
+    in this way, i.e. not under the GNU General Public License,
+    please contact Arageli Support Service support.arageli@gmail.com.
+
+*****************************************************************************/
+
+/**
+    \file _pattern.cpp
+    \brief <!--ADD A BRIEF FILE DESCRIPTION HERE-->
+
+    <!--ADD ADDITIONAL FILE DESCRIPTION HERE-->
+*/
+
+
+// PLACE ALL IMPLEMENTATIONS HERE
