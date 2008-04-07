9d48f43d8fa53fa84ab0451bb545033f0afb0950
slyalin <slyalin@f7bf301e-a539-0410-8f40-a4c9e81c2584>
2008-04-07 21:26:30 +0000

    Added two new source files to implement sparse_matrix template class in them.

---
+++ src/arageli/sparse_matrix.hpp
@@ -0,0 +1,62 @@
+/*****************************************************************************
+
+    sparse_matrix.hpp
+
+    This file is a part of the Arageli library.
+
+    Copyright (C) 2005--2008 Sergey S. Lyalin
+    University of Nizhni Novgorod, Russia
+
+    The Arageli Library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    The Arageli Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
+    We are also open for dual licensing for the whole library or
+    for its particular part. If you are interested to get the library
+    in this way, i.e. not under the GNU General Public License,
+    please contact Arageli Support Service support.arageli@gmail.com.
+
+*****************************************************************************/
+
+/**
+    \file sparse_matrix.hpp
+    \brief Sparse matrix and supporting tools declaration.
+
+    <!--ADD ADDITIONAL FILE DESCRIPTION HERE-->
+*/
+
+
+#ifndef _ARAGELI_sparse_matrix_hpp_
+#define _ARAGELI_sparse_matrix_hpp_
+
+#include "config.hpp"
+
+// REFERENCE ADDITIONAL HEADERS HERE
+
+
+namespace Arageli
+{
+
+
+// PLACE ALL DECLARATIONS AND INLINE IMPLEMENTATIONS HERE
+
+
+} // namesapce Arageli
+
+
+#ifdef ARAGELI_INCLUDE_CPP_WITH_EXPORT_TEMPLATE
+    #define ARAGELI_INCLUDE_CPP_WITH_EXPORT_TEMPLATE_sparse_matrix
+    #include "sparse_matrix.cpp"
+    #undef  ARAGELI_INCLUDE_CPP_WITH_EXPORT_TEMPLATE_sparse_matrix
+#endif
+
+#endif    // #ifndef _ARAGELI_sparse_matrix_hpp_
