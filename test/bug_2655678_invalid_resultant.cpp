168a5b695252cbf5cff6bfff0162a0f0e243a47c
slyalin <slyalin@f7bf301e-a539-0410-8f40-a4c9e81c2584>
2010-06-10 12:58:18 +0000

    Added concrete test stated in bug #2655678 description. It fails.

---
+++ test/bug_2655678_invalid_resultant.cpp
@@ -0,0 +1,79 @@
+/*****************************************************************************
+
+    test/bug_2655678_invalid_resultant.cpp
+
+    This file is a part of the Arageli library test base.
+
+    Copyright (C) 2010 Sergey S. Lyalin
+    University of Nizhni Novgorod, Russia
+
+    The Arageli Library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    The Arageli Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
+    We are also open for dual licensing for the whole library or
+    for its particular part. If you are interested to get the library
+    in this way, i.e. not under the GNU General Public License,
+    please contact Arageli Support Service support.arageli@gmail.com.
+
+*****************************************************************************/
+
+/**
+    \file bug_2655678_invalid_resultant.cpp
+    \brief This file includes test for bug #2655678.
+
+    To test bug #2655678 "Invalid work of resultant() function" and related
+    stuff.
+*/
+
+
+#include "stdafx.hpp"
+
+using namespace Arageli;
+
+
+namespace
+{
+
+// PLACE AUXILIARY CODE HERE
+
+}
+
+
+TEST_FUNCTION
+(
+    resultant_bug_2655678,
+    "Test bug #2655678 \"Invalid work of resultant() function.\""
+)
+{
+    bool is_ok = true;
+
+    ARAGELI_TS_ALLEXCEPT_CATCH_REGION_BEGIN
+    {
+        sparse_polynom<big_int> pol1 = "2*x^2+x+1";
+        sparse_polynom<big_int> pol2 = "4*x+1";
+
+        big_int resA = resultant(pol1, pol2);
+        big_int resB = resultant(pol2, pol1);
+
+        if(resA != resB)
+        {
+            is_ok = false;
+            tout
+                << "resultant(" << pol1 << ", " << pol2 << ") == " << resA << " != "
+                << resB << " == resultant(" << pol2 << ", " << pol1 << ")\n";
+        }
+    }
+    ARAGELI_TS_ALLEXCEPT_CATCH_REGION_END
+
+    return is_ok ? resOK : resFAIL;
+}
