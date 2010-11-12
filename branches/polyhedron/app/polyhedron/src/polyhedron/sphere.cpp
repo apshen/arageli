ee0ec4f3423fab354650fc32ab5f2b6c759376a8
slyalin <slyalin@f7bf301e-a539-0410-8f40-a4c9e81c2584>
2010-11-12 01:23:55 +0000

    Added repulsion key and implemented it for random points on sphere. Added OUTPUT_VRML internal macro to generate vrml files for sphere random repulsion mode.

--- branches/polyhedron/app/polyhedron/src/polyhedron/sphere.cpp
+++ branches/polyhedron/app/polyhedron/src/polyhedron/sphere.cpp
@@ -42,6 +42,7 @@ namespace polyhedron
 
 
 #define HOLD_AS_MATRIX 0
+//#define OUTPUT_VRML
 
 void Sphere::generate (std::ostream& out, CmdArgs& cmdargs) const
 {
@@ -57,13 +58,21 @@ void Sphere::generate (std::ostream& out, CmdArgs& cmdargs) const
 
         out << r << ' ' << dim+1 << '\n';
 
-        out << std::setprecision(6);
+        out << std::setprecision(15);
 
 #if HOLD_AS_MATRIX
 
         matrix<rational<> > mat(r, dim);
 
 #endif
+
+        matrix<double> repulsion_matrix;
+#ifndef OUTPUT_VRML
+        if(cmdargs.repulsion.getValue() > 0)
+#endif
+        {
+            repulsion_matrix.resize(r, dim);
+        }
 
         vector<double> val(dim);
         unsigned int i = 0;
@@ -79,19 +88,29 @@ void Sphere::generate (std::ostream& out, CmdArgs& cmdargs) const
             if (len < 1.0)
             {
                 val /= len;
-                out << '1';
-                for (unsigned int j = 0; j < dim; ++j)
+                #ifndef OUTPUT_VRML
+                if(cmdargs.repulsion.getValue() > 0)
+                #endif
                 {
-#if HOLD_AS_MATRIX
-
-                    mat(i, j) = val[j];
-#endif
-                    out << ' ' << val[j];
+                    repulsion_matrix.assign_row(i, val);
                 }
-                out << '\n';
-
-                ++i;
+                #ifndef OUTPUT_VRML
+                else
+                #endif
+                {
+                    out << '1';
+                    for (unsigned int j = 0; j < dim; ++j)
+                    {
+#if HOLD_AS_MATRIX
+                        mat(i, j) = val[j];
+#endif
+                        out << ' ' << val[j];
+                    }
+                    out << '\n';
+                }
             }
+
+            ++i;
         }
 
 #if HOLD_AS_MATRIX
@@ -103,6 +122,65 @@ void Sphere::generate (std::ostream& out, CmdArgs& cmdargs) const
 
 #endif
 
+        #ifndef OUTPUT_VRML
+        if(cmdargs.repulsion.getValue() > 0)
+        #endif
+        {
+            ARAGELI_ASSERT_1(repulsion_matrix.nrows() == r);
+            ARAGELI_ASSERT_1(repulsion_matrix.ncols() == dim);
+
+            double force = 1.0/r;   // start force adjacement multiplier
+
+            for(int i = 0; i < cmdargs.repulsion.getValue(); ++i, force *= 0.99)
+            {
+                double maxshift = 0;
+                double mindist = 3;
+
+                for(int j = 0; j < r; ++j)
+                {
+                    vector<double> currow = repulsion_matrix.copy_row(j);
+                    vector<double> shift(dim);
+
+                    for(int k = 0; k < r; ++k)
+                        if(k != j)
+                        {
+                            vector<double> dir = currow - repulsion_matrix.copy_row(k);
+                            double dot = dotsquare(dir);
+                            if(dot)
+                            {
+                                shift += dir*(force/dot);
+                                //std::cout << std::sqrt(dot) << "  ";
+                                mindist = std::min(mindist, std::sqrt(dot));
+                            }
+                        }
+
+                        //output_aligned(std::cout, repulsion_matrix);
+
+                    vector<double> newrow = currow + shift;
+                    newrow /= std::sqrt(dotsquare(newrow));
+                    repulsion_matrix.assign_row(j, newrow);
+                    maxshift = std::max(maxshift, std::sqrt(dotsquare(currow - newrow)));
+                }
+
+                std::cout << "Repulsion iteration " << i + 1 << ", maxshift = " << maxshift << ", mindist = " << mindist << '\n';
+            }
+
+            for(int i = 0; i < r; ++i)
+            {
+                out << "1  ";
+                for(int j = 0; j < dim; ++j)
+                    out << repulsion_matrix(i, j) << ' ';
+                out << '\n';
+            }
+
+            #ifdef OUTPUT_VRML
+            {
+                Arageli::polyhedron<> p(matrix<rational<> >(repulsion_matrix), fromvert);
+                std::ofstream file("polyhedron.wrl");
+                output_vrml(file, p);
+            }
+            #endif
+        }
     }
     else
     {
