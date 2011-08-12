8d5ef66d9692d9b5fcc17cf4d219acc8a260f88b
Alexander Pshenichnikov <alexander.pshenichnikov@gmail.com>
2011-08-12 16:45:16 +0000

    Fixed warning "#pragma once in main file" when building with gcc under Linux. Library and tests should compile without any warnings now

--- test/stdafx.hpp
+++ test/stdafx.hpp
@@ -31,7 +31,8 @@
 // are changed infrequently
 //
 
-#pragma once
+#ifndef _ARAGELI_TEST_stdafx_hpp
+#define _ARAGELI_TEST_stdafx_hpp
 
 #include <arageli/config.hpp>
 
@@ -94,3 +95,5 @@
 
 #pragma warning(disable : 4018)
 #pragma warning(disable : 4800)
+
+#endif
